17a10aad09e87e8422029f4bbb5966e89c3392e9
Brian Lu <me@greencappuccino.net>
2023-06-14 21:29:33 -0400

    conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,9 @@ function fish_right_prompt
     set -l toolbox (basename "$TOOLBOX_PATH")
     printf '%s' "$toolbox"
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /var/home/cappy/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
